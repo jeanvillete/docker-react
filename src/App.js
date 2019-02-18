315e8a3ce9134ebea9aa5bf6c9b3b5a44a7b76d1
Jean Villete <jean.villete@gmail.com>
2019-02-17 22:36:47 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+					<p>
+					 Another change, now meanwhile running with docker-compose!
+					</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
